489029165ebe5253c43fccda8563ea3b212fbfef
Matthew0314 <matthewh314@gmail.com>
2026-07-09 16:01:40 -0400

    Installed necessary files and tools

---
+++ backend/src/main/java/com/StorySmith/Story_Smith/StorySmithApplication.java
@@ -0,0 +1,13 @@
+package com.StorySmith.Story_Smith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorySmithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorySmithApplication.class, args);
+	}
+
+}
